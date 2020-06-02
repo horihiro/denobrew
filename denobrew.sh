0f801e364b50665df66194efaeaf1640b5b70a54
horihiro <hori.kawa.shima.tool+github@gmail.com>
2020-06-02 20:35:24 +0900

    rename to denobrew

--- denobrew.sh
+++ denobrew.sh
@@ -3,9 +3,9 @@
 
 DENO_RELEASE_URL="https://api.github.com/repos/denoland/deno/releases?per_page=100"
 
-DVM_HOME="${HOME}/.dvm.sh"
-DVM_RELEASE="${DVM_HOME}/releases"
-DVM_CACHE="${DVM_HOME}/cache"
+DENOBREW_HOME="${HOME}/.denobrew"
+DENOBREW_RELEASE="${DENOBREW_HOME}/releases"
+DENOBREW_CACHE="${DENOBREW_HOME}/cache"
 
 SUBCOMMANDS=(
   "ls"
@@ -57,13 +57,13 @@ do
   }
 done
 
-function dvm-ls-remote () {
-  if [ -n "${DVMSH_GITHUBAPI_CREDENTIAL}" ]; then
-    releases=$(curl -u "${DVMSH_GITHUBAPI_CREDENTIAL}" -fsSL "${DENO_RELEASE_URL}")
+function denobrew-ls-remote () {
+  if [ -n "${DENOBREWSH_GITHUBAPI_CREDENTIAL}" ]; then
+    releases=$(curl -u "${DENOBREWSH_GITHUBAPI_CREDENTIAL}" -fsSL "${DENO_RELEASE_URL}")
   else
     releases=$(curl -fsSL "${DENO_RELEASE_URL}" 2>/dev/null)
     if [ -z "${releases}" ]; then
-      echo_red "API rate limit might exceeded \nPlease set GitHub.com credential to \${DVMSH_GITHUBAPI_CREDENTIAL}" >&2
+      echo_red "API rate limit might exceeded \nPlease set GitHub.com credential to \${DENOBREWSH_GITHUBAPI_CREDENTIAL}" >&2
       return
     fi
   fi
@@ -74,14 +74,14 @@ function dvm-ls-remote () {
   fi
 }
 
-function dvm-ls () {
-  bins=$(ls -r ${DVM_RELEASE}/*/bin/deno 2>/dev/null)
+function denobrew-ls () {
+  bins=$(ls -r ${DENOBREW_RELEASE}/*/bin/deno 2>/dev/null)
   installed=()
   for bin in ${bins}
   do
     v="$(${bin} --version | grep deno)"
     v=v${v#deno }
-    e=${bin#${DVM_RELEASE}/}
+    e=${bin#${DENOBREW_RELEASE}/}
     e=${e%/bin/deno}
     if [ "${e}" = ${v} ]; then
       installed=("${installed[@]}" ${v})
@@ -102,38 +102,38 @@ function dvm-ls () {
   echo "current: ${v}"
 }
 
-function dvm-ls-all () {
+function denobrew-ls-all () {
   echo "Remote:"
-  dvm-ls-remote
+  denobrew-ls-remote
   echo ""
   echo "Local:"
-  dvm-ls
+  denobrew-ls
 }
 
-function dvm-install () {
+function denobrew-install () {
   if [ -z "$1" ]; then
     echo_red "Please set version string (ex. \`v1.0.0\`)." >&2
     exit 1;
   fi
-  deno_version=$(dvm-ls-remote --flat| grep -x "$1" || echo "")
+  deno_version=$(denobrew-ls-remote --flat| grep -x "$1" || echo "")
   if [ "${deno_version}" != "$1" ]; then
     echo_red "Deno \`$1\` is not found in Deno releases." >&2;
     echo_red "Please check released versions by \`$(basename $0) ls-remote\`." >&2
     exit 1;
   fi
-  DENO_INSTALL=${DVM_RELEASE}/${deno_version}
+  DENO_INSTALL=${DENOBREW_RELEASE}/${deno_version}
   mkdir -p ${DENO_INSTALL}
-  mkdir -p ${DVM_CACHE}/${deno_version}
+  mkdir -p ${DENOBREW_CACHE}/${deno_version}
   curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=${DENO_INSTALL} sh -s ${deno_version} | head -n 3
   echo_blue "Please execute \`$(basename $0) use ${deno_version}\` for activating the version." >&2
 }
 
-function dvm-use () {
+function denobrew-use () {
   if [ -z "$1" ]; then
     echo_red "Please set version string (ex. \`v1.0.0\`)." >&2
     exit 1;
   fi
-  deno_version=$(dvm-ls --flat | grep -x "$1" || echo "")
+  deno_version=$(denobrew-ls --flat | grep -x "$1" || echo "")
   if [ "${deno_version}" != "$1" ]; then
     echo_red "Deno \`$1\` is not found in your machine." >&2
     echo_red "Please retry after executing following command." >&2
@@ -144,7 +144,7 @@ function dvm-use () {
   deno_install=${DENO_INSTALL:-${HOME}/.deno}/bin
   unlink ${deno_install} 2>/dev/null
   mkdir -p $(dirname ${deno_install})
-  ln -s "${DVM_RELEASE}/${deno_version}/bin" ${deno_install}
+  ln -s "${DENOBREW_RELEASE}/${deno_version}/bin" ${deno_install}
 
   which deno 2>&1 1>/dev/null || {
     echo_red "Please add \`${deno_install}\` to PATH and do again." >&2
@@ -154,17 +154,17 @@ function dvm-use () {
   deno_dir=${deno_dir//\"/}
   unlink ${deno_dir} 2>/dev/null || rm -rf ${deno_dir} 2>/dev/null
   mkdir -p $(dirname ${deno_dir})
-  ln -s "${DVM_CACHE}/${deno_version}/" ${deno_dir}
+  ln -s "${DENOBREW_CACHE}/${deno_version}/" ${deno_dir}
 
   deno --version 2>/dev/null || echo_red "Please add \`${deno_install}\` to PATH." >&2
 }
 
-function dvm-uninstall () {
+function denobrew-uninstall () {
   if [ -z "$1" ]; then
     echo_red "Please set version string (ex. \`v1.0.0\`)." >&2
     exit 1;
   fi
-  deno_uninstall_version=$(dvm-ls --flat | grep -x "$1" || echo "")
+  deno_uninstall_version=$(denobrew-ls --flat | grep -x "$1" || echo "")
   if [ "${deno_uninstall_version}" != "$1" ]; then
     echo_red "Deno \`$1\` is not found in your machine." >&2
     exit 1;
@@ -177,8 +177,8 @@ function dvm-uninstall () {
     unlink ${deno_dir} 2>/dev/null || rm -rf ${deno_dir}
     unlink ${DENO_INSTALL:-${HOME}/.deno}/bin 2>/dev/null
   fi
-  rm -rf "${DVM_RELEASE}/${deno_uninstall_version}/"
-  rm -rf "${DVM_CACHE}/${deno_uninstall_version}/"
+  rm -rf "${DENOBREW_RELEASE}/${deno_uninstall_version}/"
+  rm -rf "${DENOBREW_CACHE}/${deno_uninstall_version}/"
 }
 
 if [ -z "$1" ]; then
@@ -187,12 +187,12 @@ if [ -z "$1" ]; then
   echo ""
   if [ "${subCmd}" = "use" -o "${subCmd}" = "uninstall" ]; then
     echo "installed versions:"
-    dvm-ls >&2
+    denobrew-ls >&2
     echo ""
     read -p "version: " version
   elif [ "${subCmd}" = "install" ]; then
     echo "released versions:"
-    dvm-ls-remote >&2
+    denobrew-ls-remote >&2
     echo ""
     read -p "version: " version
   fi
@@ -207,5 +207,5 @@ if ! $(echo ${SUBCOMMANDS[@]} | grep -q -w ${subCmd[0]} || echo false) ; then
   exit 1
 fi
 
-dvm-"${subCmd[@]}"
+denobrew-"${subCmd[@]}"
 
